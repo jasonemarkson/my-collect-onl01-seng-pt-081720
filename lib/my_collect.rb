74a20ebe90360bc9b204e158b558684d9aee2e11
Jason Markson <jasonmarkson76@gmail.com>
2020-08-27 01:18:05 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,12 +2,9 @@ def my_collect(languages)
   i = 0 
   while i < languages.length 
   collection = []
-  collection << yield(languages[i])
+  collection << yield(languages.upcase[i])
   i += 1 
 end 
   collection
 end
 
-my_collect(collection) do |lang| 
-  lang.upcase
-end
